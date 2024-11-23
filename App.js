c89d41dee115e8170fdcba4d2f88a1dbc7349cfa
Rushabh Sagara <sagararushabh@gmail.com>
2024-11-23 16:46:59 +0530

    Adding JSX and React Components and Component Composition

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am h1 tag!"),
-    React.createElement("h2", {}, "I am h2 tag!"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am h1 tag!"),
-    React.createElement("h2", {}, "I am h2 tag!"),
-  ]),
-]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Heading = () => (
+  <h1 className="head" tabIndex="5">
+    Hi from JSX!
+  </h1>
+);
+
+const HeadingComponent = () => (
+  // return (
+    <div id="container">
+      {<Heading />}
+      {Heading()}
+      <Heading />
+      <Heading></Heading>
+      <h1 className="heading">This is React Component!</h1>
+    </div>
+  );
+// };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
