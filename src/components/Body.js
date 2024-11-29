b6f2efc9f34233bb6f962707774b2184c4219da5
Rushabh Sagara <sagararushabh@gmail.com>
2024-11-29 14:21:29 +0530

    Creating a search-bar for searching restaurants by name

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,9 @@ import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
+  const [originalData, setOriginalData] = useState([]);
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -16,23 +18,64 @@ const Body = () => {
 
     const jsonData = await response.json();
 
-    setListOfRestaurants(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    setListOfRestaurants(
+      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setOriginalData(
+      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
   };
 
-  return listOfRestaurants.length === 0 ? <Shimmer /> : (
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
-        <button
-          className="filter-btn"
-          onClick={() => {
-            const newList = listOfRestaurants.filter(
-              (res) => res.info.avgRating > 4.4
-            );
-            setListOfRestaurants(newList);
-          }}
-        >
-          Top Restaurants!
-        </button>
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            placeholder="Search..."
+            value={searchValue}
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              const newList = originalData.filter((res) =>
+                res.info.name.toLowerCase().includes(searchValue.toLowerCase())
+              );
+              setListOfRestaurants(newList);
+            }}
+          >
+            Search
+          </button>
+          <button
+            className="all-res-btn"
+            onClick={() => {
+              setListOfRestaurants(originalData);
+            }}
+          >
+            All the Restaurants
+          </button>
+        </div>
+        <div className="top-filter">
+          <button
+            className="filter-btn"
+            onClick={() => {
+              const newList = listOfRestaurants.filter(
+                (res) => res.info.avgRating > 4.4
+              );
+              setListOfRestaurants(newList);
+            }}
+          >
+            Top Restaurants!
+          </button>
+        </div>
       </div>
       <div className="res-container">
         {listOfRestaurants.map((Restaurant) => (
